3459bfdf8039a3baba509f3e5ccf200fa459aec3
Levi Joyce <linuxgamer82@gmail.com>
2024-05-06 12:59:47 -0400

    list of students added

--- GradeCalc/Program.cs
+++ GradeCalc/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace GradeCalc
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace GradeCalc
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           List<Student> Students = new List<Student>();
+
+            Student Jim = new Student();
+            Student Bob= new Student();
+            Student Billy = new Student();
+            Student Chet = new Student();
+
+            Students.Add(Jim);
+            Students.Add(Bob);
+            Students.Add(Chet);
+            Students.Add(Bob);
+
         }
     }
 }
